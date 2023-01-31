d5e0c3643264f81792c4434d9a8436ea4b7b88a3
Heisenbilin <43266111+Heisenbilin@users.noreply.github.com>
2023-01-31 15:59:54 +0800

    update: vue2 component

--- packages/components/src/chart/basicChart.vue
+++ packages/components/src/chart/basicChart.vue
@@ -2,54 +2,107 @@
   <div :class="className" ref="chartRef" :style="{ height, width }"></div>
 </template>
 
-<script setup lang="ts">
-import { Ref, watch, ref } from 'vue'
+<script lang="ts">
 import { useECharts } from '@vben/hooks'
+import { defineComponent, ref, watch, Ref } from 'vue'
+export default defineComponent({
+  props: {
+    className: {
+      type: String,
+      default: 'chart',
+    },
+    width: {
+      type: String,
+      default: '100%',
+    },
+    height: {
+      type: String,
+      default: '300px',
+    },
+    autoResize: {
+      type: Boolean,
+      default: true,
+    },
+    chartOption: {
+      type: Object,
+      required: true,
+      default: () => ({}),
+    },
+    bindFunctions: {
+      type: Object,
+      required: false,
+      default: () => ({}),
+    },
+    zrFunctions: {
+      type: Object,
+      required: false,
+      default: () => ({}),
+    },
+  },
+  setup(props) {
+    const chartRef = ref<HTMLDivElement | null>(null)
+    const { setOptions, setActions } = useECharts(chartRef as Ref<HTMLDivElement>)
 
-const props = defineProps({
-  className: {
-    type: String,
-    default: 'chart',
-  },
-  width: {
-    type: String,
-    default: '100%',
-  },
-  height: {
-    type: String,
-    default: '300px',
-  },
-  autoResize: {
-    type: Boolean,
-    default: true,
-  },
-  chartOption: {
-    type: Object,
-    required: true,
-    default: () => ({}),
-  },
-  bindFunctions: {
-    type: Object,
-    required: false,
-    default: () => ({}),
-  },
-  zrFunctions: {
-    type: Object,
-    required: false,
-    default: () => ({}),
+    watch(
+      () => props.chartOption,
+      obj => {
+        setOptions(obj)
+        // 添加传入的鼠标事件
+        setActions(props.bindFunctions, props.zrFunctions)
+      },
+      { deep: true, immediate: true },
+    )
+
+    return {
+      chartRef,
+    }
   },
 })
 
-const chartRef = ref<HTMLDivElement | null>(null)
-const { setOptions, setActions } = useECharts(chartRef as Ref<HTMLDivElement>)
+// const props = defineProps({
+//   className: {
+//     type: String,
+//     default: 'chart',
+//   },
+//   width: {
+//     type: String,
+//     default: '100%',
+//   },
+//   height: {
+//     type: String,
+//     default: '300px',
+//   },
+//   autoResize: {
+//     type: Boolean,
+//     default: true,
+//   },
+//   chartOption: {
+//     type: Object,
+//     required: true,
+//     default: () => ({}),
+//   },
+//   bindFunctions: {
+//     type: Object,
+//     required: false,
+//     default: () => ({}),
+//   },
+//   zrFunctions: {
+//     type: Object,
+//     required: false,
+//     default: () => ({}),
+//   },
+// })
 
-watch(
-  () => props.chartOption,
-  obj => {
-    setOptions(obj)
-    // 添加传入的鼠标事件
-    setActions(props.bindFunctions, props.zrFunctions)
-  },
-  { deep: true, immediate: true },
-)
+// const chartRef = ref<HTMLDivElement | null>(null)
+// const { setOptions, setActions } = useECharts(chartRef as Ref<HTMLDivElement>)
+
+// watch(
+//   () => props.chartOption,
+//   obj => {
+//     setOptions(obj)
+//     // 添加传入的鼠标事件
+//     setActions(props.bindFunctions, props.zrFunctions)
+//   },
+//   { deep: true, immediate: true },
+// )
 </script>
