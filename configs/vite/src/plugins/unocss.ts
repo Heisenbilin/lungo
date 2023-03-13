ed7a81836650b2979ac365678d8554a6264ec4ff
Heisenbilin <43266111+Heisenbilin@users.noreply.github.com>
2023-03-13 16:18:30 +0800

    fix: 日期选择本地化、数据展示，数据项提示

--- configs/vite/src/plugins/unocss.ts
+++ configs/vite/src/plugins/unocss.ts
@@ -11,8 +11,7 @@ export function configUnocssPlugin() {
     presets: [presetIcons(), presetMini({ dark: 'class' })],
     shortcuts: {
       'flex-center': 'flex justify-center items-center',
-      'chart-title':
-        'w-full text-base text-left ml-2 pl-2 font-bold border-l-4 border-blue-700 inline',
+      'chart-title': 'text-base ml-2 pl-2 font-bold border-l-4 border-blue-700',
       'filter-tag': 'font-medium !mr-10 float-right !opacity-70',
       'chart-container': 'bg-$component-background-color rounded-lg p-3 col-span-2 xl:col-span-1',
       'chart-container-full': 'chart-container !col-span-2',
